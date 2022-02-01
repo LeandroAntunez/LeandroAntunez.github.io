baf93e8fbb41d0791cd8d070456f1dd95a9b8ada
Leandro Antunez <leandro_antunez@outlook.com>
2022-02-01 17:48:37 -0300

    Navbar toggle button now works!

--- src/PorfolioContainer/NavBar/NavBar.js
+++ src/PorfolioContainer/NavBar/NavBar.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
 
-class NavBar extends React.Component {
+function NavBar() {
+    const [isActive, setActive] = useState(false);
 
-    render() {
-        return (
-            <div>
-                <nav className="navbar">
-                    <div className="max-width">
-                        <div className="logo"><a href='#'>Portfo<span>lio.</span></a></div>
-                        <ul className="menu">
-                            <li><a href='#profile'>Perfil</a></li>
-                            <li><a href='#projects'>Proyectos</a></li>
-                            <li><a href='#skills'>Habilidades</a></li>
-                            <li><a href='#contact'>Contáctame</a></li>
-                        </ul>
+    const toggleClass = () => {
+        setActive(!isActive);
+    };
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="max-width">
+                    <div className="logo"><a href='#profile'>Portfo<span>lio.</span></a></div>
+                    <ul className={isActive ? 'menu active' : 'menu'} >
+                        <li><a href='#profile'>Perfil</a></li>
+                        <li><a href='#projects'>Proyectos</a></li>
+                        <li><a href='#skills'>Habilidades</a></li>
+                        <li><a href='#contact'>Contáctame</a></li>
+                    </ul>
+                    <div className='menu-btn' onClick={toggleClass}>
+                        <i class={isActive ? "fas fa-bars active" : "fas fa-bars"}></i>
                     </div>
-                </nav>
-            </div>
-        );
-    }
+                </div>
+            </nav>
+        </div>
+    );
+
 }
 
 export default NavBar;
