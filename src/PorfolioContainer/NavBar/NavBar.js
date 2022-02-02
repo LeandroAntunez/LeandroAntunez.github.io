7c9364e8406743d27cae84a59a2b741f6a73c08c
Leandro Antunez <leandro_antunez@outlook.com>
2022-02-02 19:12:36 -0300

    By click on links from navbar, disable the toggle.

--- src/PorfolioContainer/NavBar/NavBar.js
+++ src/PorfolioContainer/NavBar/NavBar.js
@@ -13,10 +13,10 @@ function NavBar() {
                 <div className="max-width">
                     <div className="logo"><a href='#profile'>Portfo<span>lio.</span></a></div>
                     <ul className={isActive ? 'menu active' : 'menu'} >
-                        <li><a href='#profile'>Perfil</a></li>
-                        <li><a href='#projects'>Proyectos</a></li>
-                        <li><a href='#skills'>Habilidades</a></li>
-                        <li><a href='#contact'>Contáctame</a></li>
+                        <li><a href='#profile' onClick={toggleClass}>Perfil</a></li>
+                        <li><a href='#projects' onClick={toggleClass}>Proyectos</a></li>
+                        <li><a href='#skills' onClick={toggleClass}>Habilidades</a></li>
+                        <li><a href='#contact' onClick={toggleClass}>Contáctame</a></li>
                     </ul>
                     <div className='menu-btn' onClick={toggleClass}>
                         <i class={isActive ? "fas fa-bars active" : "fas fa-bars"}></i>
